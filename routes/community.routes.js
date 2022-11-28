5dc21f8dfdf982694cac511de407a7fa902ff5c4
carlota-taveira <114014431+carlota-taveira@users.noreply.github.com>
2022-11-28 19:09:55 +0000

    routescommunity

--- routes/community.routes.js
+++ routes/community.routes.js
@@ -8,13 +8,14 @@ const { isAuthenticated } = require("../middleware/jwt.middleware");
 const Community = require("../models/Community");
 
 
+
 //GET ALL events from the API
 router.get("/events", async (req, res, next) => {
   try {
     let response = await axios.get("http://culturaportugal.gov.pt/umbraco/api/eventsapi/GetEvents")
     let eventsAll = response.data
 
- 
+
     res.status(200).json(eventsAll)
   } catch (error) {
     //console.log(error)
@@ -22,8 +23,6 @@ router.get("/events", async (req, res, next) => {
   }
 });
 
-
-// GET /api/community/event-search - Find an Event by name
 //Event details FROM THE API
 router.get("/event-search", async (req, res, next) => {
   try {
@@ -40,7 +39,7 @@ router.get("/event-search", async (req, res, next) => {
   }
 });
 
-// POST /api/community/event-search - Find an Event by name
+// POST - create an event with ID in our database
 router.post("/events", isAuthenticated, async (req, res, next) => {
   const { Name } = req.query
   const userId = req.payload._id // na rota como parametro //req.payload._id
@@ -52,9 +51,9 @@ router.post("/events", isAuthenticated, async (req, res, next) => {
     let event = allEvents.filter((events) => events.Name === Name)[0]
 
     //check if it exists 
-    let eventExists = await Event.findOne({title: event.Name})
+    let eventExists = await Event.findOne({ title: event.Name })
 
-    if(eventExists){
+    if (eventExists) {
       res.json(eventExists);
       return;
     }
@@ -65,10 +64,6 @@ router.post("/events", isAuthenticated, async (req, res, next) => {
       where: event.Where, price: event.Price, info: event.Info, link: event.Url
     }) // primeiro nome do model, segundo nome do API
     console.log(attendEvent)
-    
-    // we will need to create a route that will direct everything to Community and will allow the
-    // user to have attendancy counted 
-    //populate?
 
     res.status(200).json(attendEvent)
 
@@ -80,16 +75,16 @@ router.post("/events", isAuthenticated, async (req, res, next) => {
 
 //Get details
 //From the DB!
-router.get('/events/:id', async(req, res, next) => {
-try {
-  const {id} = req.params
+router.get('/events/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
 
-  let eventDetails = await Event.findById(id).populate("attendance comments")
-  res.status(200).json(eventDetails)
-} catch (error) {
-  res.json(error)
-  next(error)
-}
+    let eventDetails = await Event.findById(id).populate("attendance comments")
+    res.status(200).json(eventDetails)
+  } catch (error) {
+    res.json(error)
+    next(error)
+  }
 })
 
 
@@ -104,11 +99,11 @@ router.post('/events/create-comment/:id', isAuthenticated, async (req, res, next
     const newComment = await Comment.create({ comments, user: user._id });
 
     // Push comment to the event
-     const updateEvent = await Event.findByIdAndUpdate(id, {
+    const updateEvent = await Event.findByIdAndUpdate(id, {
       $push: {
-      comments: newComment._id,
-    },
-  }) 
+        comments: newComment._id,
+      },
+    })
     // Push comment to the User
     const updateUser = await User.findByIdAndUpdate(user._id, { $push: { comments: newComment._id }, }, { new: true })
     res.status(200).json(updateUser)
@@ -138,17 +133,34 @@ router.delete('/events/delete-comment/:id', isAuthenticated, async (req, res, ne
 
 //Attendance
 
-router.put('/events/attend/:id', isAuthenticated, async(req, res, next)=> {
+router.put('/events/attend/:id', isAuthenticated, async (req, res, next) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const userId = req.payload._id
 
-    const updateEvent = await Event.findByIdAndUpdate(id, {$push: {attendance: userId}});
+    const updateEvent = await Event.findByIdAndUpdate(id, { $push: { attendance: userId } });
 
-    const updatedUser = await User.findByIdAndUpdate(userId, {$push: {atendeeEvent: id}}, {new: true})
+    const updatedUser = await User.findByIdAndUpdate(userId, { $push: { atendeeEvent: id } }, { new: true })
 
     res.status(200).json(updatedUser)
+
+  } catch (error) {
+    res.json(error)
+    next(error)
+  }
+})
+
+router.put('/events/favorite/:id', isAuthenticated, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const userId = req.payload._id
+
+    const favourite = await Event.findByIdAndUpdate(id, {$push: {favorite: userId}});
     
+    const updatedUser = await User.findByIdAndUpdate(userId, { $push: { favorite: id } }, { new: true })
+
+    res.status(200).json(updatedUser)
+
   } catch (error) {
     res.json(error)
     next(error)
